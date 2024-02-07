ae175fcfd4a6e5c81aa5cbd8025be0d62b2326ab
dwcs-2324 <dwcs2324@gmail.com>
2024-02-07 17:40:17 +0100

    --s en itemss

--- autoload.php
+++ autoload.php
@@ -1,10 +1,11 @@
 <?php
 const CLASES_FOLDER="clases";
 const  PEOPLE_FOLDER="people";
-const  ITEMS_FOLDER="itemss";
+const  ITEMS_FOLDER="items";
 
 spl_autoload_register(function ($nombre_clase) {
 
+    //echo "## $nombre_clase<br/>";
     $dirs= [PEOPLE_FOLDER, ITEMS_FOLDER];
     foreach ($dirs as $dir) {
         $ruta = CLASES_FOLDER.DIRECTORY_SEPARATOR.$dir.DIRECTORY_SEPARATOR.$nombre_clase.'.php';
